3bc6c4b615630d68f13cc7a3472ce34b422a7392
Julien Oudot <Julien Oudot@localhost>
2011-05-19 20:50:04 +0200

    Generation du fichier XML du flux RSS etudiant et enseignant. teste de mon cote, mais le jar n'est pas integre a l'appli play. De plus l'encodage du xml est UTF8 (voir si ca pose probleme)

--- test/BasicTest.java
+++ test/BasicTest.java
@@ -1,9 +1,16 @@
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 
 import models.Enseignant;
 import models.Etudiant;
+import models.NewsFeedGen;
 import models.Scolarite;
 
+import org.jdom.Document;
+import org.jdom.Element;
+import org.jdom.JDOMException;
+import org.jdom.input.SAXBuilder;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -19,7 +26,7 @@ public class BasicTest extends UnitTest {
 		try {
 			new Enseignant("ChabanasM", "secret", "Chabanas", "Matthieu", 
 			    		new SimpleDateFormat("dd/MM/yyyy").parse("31/02/1975")).save();
-		    // Retrouver l'enseignant à partir de son login
+		    // Retrouver l'enseignant ï¿½ partir de son login
 		    Enseignant mat = Enseignant.find("byLogin", "ChabanasM").first();
 		    
 		    // Test 
@@ -35,7 +42,7 @@ public class BasicTest extends UnitTest {
 		try {
 			new Etudiant("Romain", "Durant", "Durantr", "secret",  
 			    		new SimpleDateFormat("dd/MM/yyyy").parse("31/02/1987")).save();
-		    // Retrouver l'étudiant à partir de son login
+		    // Retrouver l'ï¿½tudiant ï¿½ partir de son login
 			Etudiant etudiant = Etudiant.find("byLogin", "Durantr").first();
 		    
 		    // Test 
@@ -49,12 +56,60 @@ public class BasicTest extends UnitTest {
 	@Test
 	public void creeEtChercheScolarite() {
 		new Scolarite("lecapona", "secret").save();
-		// Retrouver l'enseignant à partir de son login
+		// Retrouver l'enseignant ï¿½ partir de son login
 		Scolarite scolarite = Scolarite.find("byLogin", "lecapona").first();
 		    
 		// Test 
 		assertNotNull(scolarite);
 		assertEquals("lecapona", scolarite.login);
+	}
+	
+	@Test
+	public void generateJDomEtudiant() {
+		NewsFeedGen.generate("Etudiant_1A_ISI_2.xml", "notes de SGBD disponibles", "1A ISI Gpe 1");
+		NewsFeedGen.generate("Etudiant_1A_ISI_2.xml", "notes de SAP disponibles", "1A ISI Gpe 1");
+		
+		File fileEtudiant = new File("Etudiant_1A_ISI_2.xml");
+		
+		
+		SAXBuilder sxb = new SAXBuilder();
+		Document document = null;
+		try {
+			document = sxb.build(fileEtudiant);
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Element racine = document.getRootElement();
+        Element channel = racine.getChild("channel");
+		// Test 
+		assertNotNull(channel);
+		assertEquals(channel.getChildren("item").size(), 2);
+	}
+	
+	@Test
+	public void generateJDomEnseignant() {
+		NewsFeedGen.generate("Enseignant_Sebastien_Viardot.xml", "Examens des 1A ISI Gpe1 a noter", "Sebastien Viardot");
+		File fileEnseignant = new File("Enseignant_Sebastien_Viardot.xml");
+		SAXBuilder sxb = new SAXBuilder();
+		Document document = null;
+		try {
+			document = sxb.build(fileEnseignant);
+		} catch (JDOMException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        Element racine = document.getRootElement();
+        Element channel = racine.getChild("channel");
+		// Test 
+		assertNotNull(channel);
+		assertEquals(channel.getChildren("item").size(), 1);
 	}
 	
 	 @Before
