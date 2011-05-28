852c1718a415eb32e1bbbbdbabd19a196e630116
Julien Oudot <Julien Oudot@localhost>
2011-05-28 10:45:09 +0200

    Deleloppement du controller pour un personnel de scolarite. Partie classe/cours/enseignant OK. RAF parties etudiant et matiere

--- app/controllers/ScolariteController.java
+++ app/controllers/ScolariteController.java
@@ -8,6 +8,7 @@ import models.Classe;
 import models.Cours;
 import models.Enseignant;
 import models.Etudiant;
+import models.Matiere;
 import models.Scolarite;
 import play.data.validation.Required;
 import play.mvc.Before;
@@ -47,15 +48,26 @@ public class ScolariteController extends Controller {
 	}
 	
 	/**
+	 * Supprimer une classe
+	 * @param id identifiant de la classe a supprimer
+	 */
+	public static void supprimerClasse(long id) {
+		Classe classe = Classe.findById(id);
+		classe._delete();
+	}
+	
+	/**
 	 * Creation/modification d'une classe
 	 * @param id identifiant de la classe a modifier
 	 */
     public static void classeForm(Long id) {
+    	// Il faut les etudiants a ajouter
+    	List<Etudiant> etudiantsSansClasse = Etudiant.sansClasse();
         if(id != null) {
             Classe classe = Classe.findById(id);
-            render(classe);
+            render(classe, etudiantsSansClasse);
         }
-        render();
+        render(etudiantsSansClasse);
     }
     
     /**
@@ -73,7 +85,12 @@ public class ScolariteController extends Controller {
 	        		String randomID) {
     	if(validation.hasErrors()) {
 	    	flash.error("Formulaire invalide");
-	        render("Scolarite/classeForm.html", Classe.findById(id), randomID);
+    		Classe classePrecharge = null;	 
+    		if (id > 0)
+    			classePrecharge = Enseignant.findById(id);
+    		else
+    			classePrecharge = new Classe(nomClasse);
+	        render("Scolarite/classeForm.html", classePrecharge, randomID);
 	    }
     	Classe classe = null;
     	if (id > 0) {
@@ -89,12 +106,265 @@ public class ScolariteController extends Controller {
     	ScolariteController.listeClasses();
     }
     
+    /**
+     * Methode de recherche d'une classe a partir d'une chaine saisie. Appelee par un fichier Javascript
+     * @param chaine chaine a rechercher parmis les noms de classe
+     * @return une chaine JSon a interpreter dans le fichier Javascipt appelant
+     */
+    public void rechercheClasseComposition(String chaine) {
+    	List<Classe> lstClasses = Classe.cherche(chaine);
+    	renderJSON(lstClasses);
+    }
+    
+	/**
+	 * Renvoi les 20 premieres matieres dans l'ordre alphabetique
+	 * */
+	public static void listeCours() {
+		List<Matiere> liste20Matieres = Matiere.find20Matiere();
+		// Booleens pour savoir si un appel a previous/next est possible
+		renderArgs.put("previous", false);
+		renderArgs.put("next", (Matiere.findAll().size() > 20));
+		render(liste20Matieres);
+	} 
+	
+	/**
+	 * Renvoi les 20 prochaines matieres dans l'ordre alphabetique
+	 * @param id identifiant de la derniere matiere affichee
+	 */
+	public static void listeNextCours(Long id) {
+		List<Matiere> liste20Matieres = Matiere.next20Matieres(id);
+		renderArgs.put("previous", true);
+		// Si la liste n'est pas pleine, c'est qu'on ne peut pas appeler encore un next
+		renderArgs.put("next", (liste20Matieres.size() == 20));
+		render("Scolarite/listeCours.html", liste20Matieres);
+	} 
+	
+	/**
+	 * Renvoi les 20 precedente matieres dans l'ordre alphabetique
+	 * @param id identifiant de la derniere matiere affichee
+	 */
+	public static void listePreviousCours(Long id) {
+		List<Matiere> liste20Matieres = Matiere.previous20Matieres(id);
+		renderArgs.put("next", true);
+		// Si la liste n'est pas pleine, c'est qu'on ne peut pas appeler encore un previous
+		renderArgs.put("previous", (liste20Matieres.size() == 20));
+		render("Scolarite/listeCours.html", liste20Matieres);
+	} 
+    
+	/**
+	 * Permet d'avoir la liste des cours d'une classe
+	 * @param id identifiant d'une classe
+	 */
+	public static void coursClasse(Long id) {
+		Classe classe = Classe.findById(id);
+		render(classe);
+	}
+	
+	/**
+	 * Supprimer un cours
+	 * @param id identifiant du cours a supprimer
+	 */
+	public static void supprimerCours(long id) {
+		Cours cours = Cours.findById(id);
+		cours._delete();
+	}
+	
+	/**
+	 * Chargement du formulaire de creation/modification d'un cours
+	 * @param idCours identifiant du cours a modifier
+	 * @param idClasse idenfitiant de la classe (utile car a conserver dans le formulaire)
+	 */
+    public static void coursForm(Long idCours, Long idClasse) {    	
+    	Matiere matiere = Matiere.findById(idClasse);
+        if(idCours != null) {
+            Cours cours = Cours.findById(idCours);
+            // Si on est dans la modification de la matiere, il faut les profs associes
+        	List<Enseignant> lstEnseignantMatiere = 
+        			Cours.enseignantByMatiere(cours.matiere);
+            render(matiere, cours, lstEnseignantMatiere);
+        }
+        // Sinon, une methode Javascript appelera l'une des deux fonctions suivantes
+        render(matiere);
+    }
+	
+    /**
+     * Appelee par une methode Javascript
+     * Renvoi en Json la liste des enseignants repertories pour la matiere
+     * @param id identifiant de la matiere selectionne (dans un menu deroulant par exemple)
+     */
+    public static void EnseignantByMatiere(Long id) {
+    	List<Enseignant> lstEtudiantMatiere = Cours.
+    			enseignantByMatiere((Matiere) Matiere.findById(id));
+    	renderJSON(lstEtudiantMatiere);
+    }
+    
+    /**
+     * Appelee par une methode Javascript
+     * Renvoi en Json la liste des enseignants qui n'enseignent pas la matiere selectionne
+     */
+    public static void allEnseignants() {
+    	List<Enseignant> lstEnseignants = Enseignant.findAllEnseignant();
+    	renderJSON(lstEnseignants);
+    }
+    
+    /**
+     * Creation/Modification d'un cours
+     * @param id identifiant du cours si modification
+     * @param matiere Matiere enseignee dans le cadre du cours
+     * @param enseignant Enseignant etant responsable du cours
+     * @param classe classe suivant le cours
+     */
+    public static void postCours(long id,
+	        @Required(message="Une matiere est requise") Matiere matiere, 
+	        @Required(message="Un enseignant est requis") Enseignant enseignant,
+	        @Required(message="Une classe est requise") Classe classe,
+	        		String randomID) {
+    	if(validation.hasErrors()) {
+	    	flash.error("Formulaire invalide");
+	        render("Scolarite/coursForm.html", Cours.findById(id), randomID);
+	    }
+    	Cours cours = null;
+    	if (id > 0) {
+    		cours = Cours.findById(id);
+    		cours.classe = classe;
+    		cours.matiere = matiere;
+    		cours.enseignant = enseignant;
+    		flash.success("Cours modifiée");
+    	} else {
+    		cours = new Cours(classe, matiere, enseignant);
+    		flash.success("Cours ajoutée");
+    	}
+    	cours.save();
+    	ScolariteController.listeCours();
+    }
+    
 	/**
 	 * Renvoi les 20 premiers enseignants dans l'ordre alphabetique
 	 * */
-	public static void listeEnseignant() {
-		List<Enseignant> liste20Enseignants = Cours.find20Enseignant(null);
+	public static void listeEnseignants() {
+		List<Enseignant> liste20Enseignants = Enseignant.find20Enseignants();
+		// Booleens pour savoir si un appel a previous/next est possible
+		renderArgs.put("previous", false);
+		renderArgs.put("next", (Enseignant.findAll().size() > 20));
 		render(liste20Enseignants);
 	} 
+	
+	/**
+	 * Renvoi les 20 prochains enseignants dans l'ordre alphabetique
+	 * @param id identifiant du dernier enseignant affiche
+	 */
+	public static void listeNextEnseignant(Long id) {
+		List<Enseignant> liste20Enseignants = Enseignant.next20Enseignants(id);
+		renderArgs.put("previous", true);
+		// Si la liste n'est pas pleine, c'est qu'on ne peut pas appeler encore un next
+		renderArgs.put("next", (liste20Enseignants.size() >= 20));
+		render("Scolarite/listeEnseignants.html", liste20Enseignants);
+	} 
+	
+	/**
+	 * Renvoi les 20 precedents enseignants dans l'ordre alphabetique
+	 * @param id identifiant du dernier enseignant affiche
+	 */
+	public static void listePreviousEnseignants(Long id) {
+		List<Enseignant> liste20Enseignants = Enseignant.previous20Enseignants(id);
+		renderArgs.put("next", true);
+		// Si la liste n'est pas pleine, c'est qu'on ne peut pas appeler encore un previous
+		renderArgs.put("previous", (liste20Enseignants.size() >= 20));
+		render("Scolarite/listeCours.html", liste20Enseignants);
+	} 
+	
+    /**
+     * Methode de recherche d'un enseignant a partir d'une chaine saisie. Appelee par un fichier Javascript
+     * @param chaine chaine a rechercher parmis les nom et prenom de l'enseignant
+     * @return une chaine JSon a interpreter dans le fichier Javascipt appelant
+     */
+    public void rechercheEnseignant(String chaine) {
+    	List<Enseignant> lstEnseignants = Enseignant.cherche(chaine);
+    	renderJSON(lstEnseignants);
+    }
+	
+	/**
+	 * Supprimer un enseignant
+	 * @param id identifiant de l'enseignant a supprimer
+	 */
+	public static void supprimerEnseignants(long id) {
+		Enseignant enseignant = Enseignant.findById(id);
+		enseignant._delete();
+	}
+	
+	/**
+	 * Chargement du formulaire de creation/modification d'un enseignant
+	 * @param idEnseignant identifiant du cours a modifier
+	 */
+    public static void enseignantForm(Long idEnseignant) {    	
+        if(idEnseignant != null) {
+        	Enseignant enseignant = Enseignant.findById(idEnseignant);
+            render(enseignant);
+        }
+        render();
+    }
     
+    /**
+     * Creation/Modification d'un enseignant
+     * @param id identifiant de l'enseignant si modification
+     * @param nom nom de l'enseignant
+     * @param prenom prenom de l'enseignant
+     * @param login login de l'enseignant (doit être unique)
+     * @param password mot de passe de l'enseignant
+     * @param passwordVerif verification de la saisie du mot de passe
+     * @param dateNaissance
+     */
+    public static void postEnseignants(long id,
+	        @Required(message="Un nom est requis") String nom, 
+	        @Required(message="Un prenom est requis") String prenom,
+	        @Required(message="Un login est requis") String login,
+	        @Required(message="Un mot de passe est requis") String password,
+	        @Required(message="Un mot de passe est requis") String passwordVerif,
+	        @Required(message="Une date de naissance est requise") Date dateNaissance,
+	        		String randomID) {
+    	if(validation.hasErrors()) {
+	    	flash.error("Formulaire invalide");
+    		Enseignant enseignantPrecharge = null;	 
+    		if (id > 0)
+    			enseignantPrecharge = Enseignant.findById(id);
+    		else
+    			enseignantPrecharge = new Enseignant(login, password, nom, prenom, dateNaissance);
+	        render("Scolarite/enseignantForm.html", enseignantPrecharge, randomID);
+	    }
+    	if (!password.equals(passwordVerif)) {
+    		flash.error("Mots de passe non identiques");
+    		Enseignant enseignantPrecharge = null;	 
+    		if (id > 0)
+    			enseignantPrecharge = Enseignant.findById(id);
+    		else
+    			enseignantPrecharge = new Enseignant(login, password, nom, prenom, dateNaissance);
+	        render("Scolarite/enseignantForm.html", enseignantPrecharge, randomID);
+    	}
+    	if (Enseignant.exist(login)) {
+    		flash.error("Le login est déjà utilisé");
+    		Enseignant enseignantPrecharge = null;
+ 
+    		if (id > 0)
+    			enseignantPrecharge = Enseignant.findById(id);
+    		else
+    			enseignantPrecharge = new Enseignant(login, password, nom, prenom, dateNaissance);
+    		
+	        render("Scolarite/enseignantForm.html", enseignantPrecharge, randomID);
+    	}
+    	Enseignant enseignant = null;
+    	if (id > 0) {
+    		enseignant = Enseignant.findById(id);
+    		enseignant.nom = nom;
+    		enseignant.prenom = prenom;
+    		enseignant.login = login;
+    		enseignant.password = password;
+    		enseignant.dateNaissance = dateNaissance;
+    		flash.success("Enseignant modifiée");
+    	} else {
+    		enseignant = new Enseignant(login, password, nom, prenom, dateNaissance);
+    		flash.success("Enseignant ajoutée");
+    	}
+    	enseignant.save();
+    	ScolariteController.listeEnseignants();
+    }
 }
