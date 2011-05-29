3ab3d50945ce5615b9251804bc5fb7799f31d9e3
Julien Oudot <Julien Oudot@localhost>
2011-05-29 14:54:52 +0200

    En cours d'integration du web service. Inclusion d'un fichier javascript

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,45 @@
 
+function loadGoogleTranslate() {
+	alert('chargement google translate');
+	google.load("language", "1");
+	alert('chargement ok');
+}
 function translateToEn() {
-	alert('translate in english');
+	//alert('translate in english');
+	google.load("language", "1");
+	alert('chargement google translate ok');
+	
+	$(".translate").each(function(n){
+		alert( 'Item #');
+		alert( 'Item #'.n );
+	 });
+	
+	google.language.translate($(".translate"), "fr", "en", function(result) {
+	  if (!result.error) {
+		var container = $(".translate");
+		container.innerHTML = result.translation;
+		alert('traduction ok');
+		alert(result.translation);
+	  }
+	});
+	
+	//alert('chargement google translate ok');
+	/*google.language.detect(text, function(result) {
+	  if (!result.error) {
+		var language = 'unknown';
+		for (l in google.language.Languages) {
+		  if (google.language.Languages[l] == result.language) {
+			language = l;
+			break;
+		  }
+		}
+		var container = document.getElementById("translate");
+		container.innerHTML = text + " is: " + language + "";
+	  }
+	});
+	google.language.detect($('#translate'), function(result));*/
+	
+	//alert('detection ok');
 }
 
 function translateToFr() {
@@ -15,7 +54,35 @@ function translateToDe() {
 	alert('translate in german');
 }
 
+/*google.language.translate("Hello world", "en", "es", function(result) {
+  if (!result.error) {
+    var container = document.getElementById("translation");
+    container.innerHTML = result.translation;
+  }
+});
+
+google.language.detect(text, function(result) {
+  if (!result.error) {
+    var language = 'unknown';
+    for (l in google.language.Languages) {
+      if (google.language.Languages[l] == result.language) {
+        language = l;
+        break;
+      }
+    }
+    var container = document.getElementById("translate");
+    container.innerHTML = text + " is: " + language + "";
+  }
+});*/
+
+//window.addEventListener("load", OnLoad, false);
 $("#translateToEn").live("click", function(){ translateToEn();});
 $("#translateToFr").live("click", function(){ translateToFr();});
 $("#translateToEs").live("click", function(){ translateToEs();});
 $("#translateToDe").live("click", function(){ translateToDe();});
+
+function OnLoad(){
+	alert('chargement google translate');
+	google.load("language", "1");
+	alert('chargement ok');
+}
