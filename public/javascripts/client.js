549488f83992798e25e91809bcb72b2f6f70aaff
Julien Oudot <Julien Oudot@localhost>
2011-05-29 17:59:55 +0200

    Ajout de l'invocation du web service. D?fonce la mise en page, sinon OK

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,88 +1,32 @@
 
-function loadGoogleTranslate() {
-	alert('chargement google translate');
-	google.load("language", "1");
-	alert('chargement ok');
-}
-function translateToEn() {
-	//alert('translate in english');
-	google.load("language", "1");
-	alert('chargement google translate ok');
-	
-	$(".translate").each(function(n){
-		alert( 'Item #');
-		alert( 'Item #'.n );
-	 });
-	
-	google.language.translate($(".translate"), "fr", "en", function(result) {
-	  if (!result.error) {
-		var container = $(".translate");
-		container.innerHTML = result.translation;
-		alert('traduction ok');
-		alert(result.translation);
-	  }
-	});
-	
-	//alert('chargement google translate ok');
-	/*google.language.detect(text, function(result) {
-	  if (!result.error) {
-		var language = 'unknown';
-		for (l in google.language.Languages) {
-		  if (google.language.Languages[l] == result.language) {
-			language = l;
-			break;
-		  }
-		}
-		var container = document.getElementById("translate");
-		container.innerHTML = text + " is: " + language + "";
-	  }
-	});
-	google.language.detect($('#translate'), function(result));*/
-	
-	//alert('detection ok');
-}
+ function translate(lang) {
+   var source = document.getElementById("initial").innerHTML;
+   var len = source.length;
 
-function translateToFr() {
-	alert('translate in french');
-}
+   // Google Language API accepts 500 characters per request 
+   var words = 500;
 
-function translateToEs() {
-	alert('translate in spanish');
-}
+   // This is for English pages, you can change the
+   // sourcelang variable for other languages
+   var sourcelang = "fr";
+   document.getElementById("translation").innerHTML = "";
 
-function translateToDe() {
-	alert('translate in german');
-}
+   for(i=0; i<=(len/words); i++) {
+     google.language.translate (source.substr(i*words, words),
+                 "fr", lang, function (result) {
+     if (!result.error) {
+     document.getElementById("translation").innerHTML
+           = document.getElementById("translation").innerHTML
+           + result.translation;
+    } }); }  
 
-/*google.language.translate("Hello world", "en", "es", function(result) {
-  if (!result.error) {
-    var container = document.getElementById("translation");
-    container.innerHTML = result.translation;
-  }
-});
-
-google.language.detect(text, function(result) {
-  if (!result.error) {
-    var language = 'unknown';
-    for (l in google.language.Languages) {
-      if (google.language.Languages[l] == result.language) {
-        language = l;
-        break;
-      }
-    }
-    var container = document.getElementById("translate");
-    container.innerHTML = text + " is: " + language + "";
-  }
-});*/
-
-//window.addEventListener("load", OnLoad, false);
-$("#translateToEn").live("click", function(){ translateToEn();});
-$("#translateToFr").live("click", function(){ translateToFr();});
-$("#translateToEs").live("click", function(){ translateToEs();});
-$("#translateToDe").live("click", function(){ translateToDe();});
-
-function OnLoad(){
-	alert('chargement google translate');
-	google.load("language", "1");
-	alert('chargement ok');
-}
+  // Hide the text written in the original language
+  document.getElementById("initial").style.display = 'none';
+  return false;
+ }
+ 
+function original() {
+  document.getElementById("translation").style.display='none';
+  document.getElementById("initial").style.display = 'block';
+  return false;
+ }
